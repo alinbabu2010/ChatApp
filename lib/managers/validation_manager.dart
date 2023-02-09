0fab7a96e0510d86a00f072a11889942b624ace4
Alin Babu <alinbabu2010@gmail.com>
2023-02-09 11:28:06 +0530

    Add form validation manager class

---
+++ lib/managers/validation_manager.dart
@@ -0,0 +1,35 @@
+import 'package:chat_app/utils/constants.dart';
+
+class ValidationManager {
+
+  static ValidationManager? _validationManager;
+
+  ValidationManager();
+
+  static ValidationManager get instance =>
+      _validationManager ?? ValidationManager();
+
+  String? isValidEmail(String? value) {
+    if (value?.isEmpty == true || value?.contains('@') == false) {
+      return Constants.emailErrorMsg;
+    }
+    return null;
+  }
+
+  String? isValidPassword(String? value) {
+    if (value?.isEmpty == true ||
+        value?.length.compareTo(8).isNegative == true) {
+      return Constants.passwordErrorMsg;
+    }
+    return null;
+  }
+
+  String? isValidUsername(String? value) {
+    if (value?.isEmpty == true ||
+        value?.length.compareTo(4).isNegative == true) {
+      return Constants.usernameErrorMsg;
+    }
+    return null;
+  }
+
+}
