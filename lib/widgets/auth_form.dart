a3f3ec967a10a42181fa2ecdb119dc10119e8eb9
Alin Babu <alinbabu2010@gmail.com>
2023-02-09 11:29:19 +0530

    Add validation and authentication switch in `AuthFrom` widget

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/managers/validation_manager.dart';
 import 'package:chat_app/utils/constants.dart';
 import 'package:chat_app/utils/dimen.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,30 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final validationManager = ValidationManager.instance;
+
+  var _isLogin = true;
+
+  String _userEmail = "";
+  String _userPassword = "";
+  String _username = "";
+
+  void _trySubmit() {
+    final isValidForm = _formKey.currentState?.validate() ?? false;
+    FocusScope.of(context).unfocus(); // Remove focus from all input fields
+    if (isValidForm) {
+      _formKey.currentState?.save();
+      print("$_username - $_userEmail - $_userPassword");
+    }
+  }
+
+  void _switchMode() {
+    setState(() {
+      _isLogin = !_isLogin;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,25 +44,34 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(Dimen.authFormCardPadding),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: const ValueKey(Constants.emailAddress),
                     keyboardType: TextInputType.emailAddress,
                     decoration: const InputDecoration(
                       labelText: Constants.emailAddress,
                       hintText: Constants.emailHint,
                     ),
                     textInputAction: TextInputAction.next,
+                    validator: validationManager.isValidEmail,
+                    onSaved: (value) => _userEmail = value ?? "",
                   ),
+                  if(!_isLogin)
                   TextFormField(
+                    key: const ValueKey(Constants.username),
                     decoration: const InputDecoration(
                       labelText: Constants.username,
                       hintText: Constants.usernameHint,
                     ),
                     textInputAction: TextInputAction.next,
+                    validator: validationManager.isValidUsername,
+                    onSaved: (value) => _username = value ?? "",
                   ),
                   TextFormField(
+                    key: const ValueKey(Constants.password),
                     decoration: const InputDecoration(
                       labelText: Constants.password,
                       hintText: Constants.passwordHint,
@@ -45,15 +79,19 @@ class _AuthFormState extends State<AuthForm> {
                     obscureText: true,
                     obscuringCharacter: Constants.obscuringCharacter,
                     textInputAction: TextInputAction.next,
+                    validator: validationManager.isValidPassword,
+                    onSaved: (value) => _userPassword = value ?? "",
                   ),
                   const SizedBox(height: Dimen.authFormBoxHeight),
                   ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(Constants.login),
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? Constants.login : Constants.signup),
                   ),
                   TextButton(
-                    onPressed: () {},
-                    child: const Text(Constants.createAccount),
+                    onPressed: _switchMode,
+                    child: Text(_isLogin
+                        ? Constants.createAccount
+                        : Constants.haveUserAccount),
                   )
                 ],
               ),
