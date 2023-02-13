a5fa48e3d818bfee3a1d346d51c800a6994c37d3
Alin Babu <alinbabu2010@gmail.com>
2023-02-13 17:45:57 +0530

    Add `MessageBubble` widget to message listView

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/managers/auth_manager.dart';
 import 'package:chat_app/utils/constants.dart';
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:chat_app/widgets/progress_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +21,16 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs?.length,
-          itemBuilder: (context, index) =>
-              Text(chatDocs?.elementAt(index)[Constants.fieldText]),
+          itemBuilder: (context, index) {
+            final chatDoc = chatDocs?.elementAt(index);
+            return MessageBubble(
+              message: chatDoc?.get(Constants.fieldText),
+              isCurrentUser: AuthManager.instance.isCurrentUser(
+                chatDoc?.get(Constants.fieldUserId),
+              ),
+              key: ValueKey(chatDoc?.id),
+            );
+          },
         );
       },
     );
