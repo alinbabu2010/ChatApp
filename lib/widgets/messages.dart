f37fa104c5317994beaee2d66e09d3a2f609348b
Alin Babu <alinbabu2010@gmail.com>
2023-02-13 13:54:42 +0530

    Add reverse parameter to true for `ListView.builder` and refactor code
    Also use manager class to access fireStore

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,6 +1,8 @@
+import 'package:chat_app/utils/constants.dart';
 import 'package:chat_app/widgets/progress_bar.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../managers/firestore_manager.dart';
 
 class Messages extends StatelessWidget {
   const Messages({Key? key}) : super(key: key);
@@ -8,16 +10,17 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      stream: FireStoreManager.instance.getChatMessages(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const ProgressBar();
         }
         final chatDocs = snapshot.data?.docs;
         return ListView.builder(
+          reverse: true,
           itemCount: chatDocs?.length,
           itemBuilder: (context, index) =>
-              Text(chatDocs?.elementAt(index)['text']),
+              Text(chatDocs?.elementAt(index)[Constants.fieldText]),
         );
       },
     );
