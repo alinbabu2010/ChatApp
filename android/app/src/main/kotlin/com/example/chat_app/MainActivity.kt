633671107ec0916addb031164acf5b4852b69fb6
Alin Babu <alinbabu2010@gmail.com>
2023-02-23 10:46:58 +0530

    Add native code for Android to get battery status

--- android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
@@ -1,6 +1,35 @@
 package com.example.chat_app
 
+import android.content.Context
+import android.os.BatteryManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+
+    private val channelName = "chat.dev/battery"
+    private val methodName = "getBatteryLevel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channelName
+        ).setMethodCallHandler { call, result ->
+            if (call.method.equals(methodName)) {
+                val batteryLevel = getBatteryLevel()
+                println(batteryLevel)
+                if (batteryLevel != -1) result.success(batteryLevel)
+                else result.error("Unavailable", "Sorry, could not fetch battery level.", null)
+
+            } else result.notImplemented()
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    }
+
 }
